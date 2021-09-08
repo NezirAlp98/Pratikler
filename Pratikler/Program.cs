a7491a68a3a1e2c6c0658edef8dd99c8487cfbd6
nezir alp arslan <nezirarslan1234@gmail.com>
2021-09-08 21:38:12 +0300

    Add project files.

---
+++ Pratikler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pratikler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İlk c# pratiğim");
+
+            Console.WriteLine("Bir ad giriniz: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Bir soyad giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Ad-soyad: " + name + " " + surname );
+
+        }
+    }
+}
