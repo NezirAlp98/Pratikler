486dad7115fcb4bee6d8533400fbb4f317fcedc3
nezir alp arslan <nezirarslan1234@gmail.com>
2021-09-13 23:14:52 +0300

    hata yönetimi eklendi

---
+++ HataYonetimi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girilen sayı: " + a);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata: " + e.Message.ToString());
+                
+            }
+
+            try
+            {
+                //int b = int.Parse(null);
+                int b = int.Parse("test");
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(e);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Yanlış değer girdiniz.");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
